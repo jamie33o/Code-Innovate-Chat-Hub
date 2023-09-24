e182a720b18aaa535368a423455813025d05a970
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-24 22:59:57 +0100

    Refactor: delete home app

--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def index(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/index.html')
