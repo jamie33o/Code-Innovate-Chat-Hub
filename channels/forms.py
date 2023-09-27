3992ef04596ee66019d482dad8fd9aa70c326184
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-27 20:25:49 +0100

    Chore: add comments

--- channels/forms.py
+++ channels/forms.py
@@ -6,6 +6,12 @@ from .models import ChannelPosts
 from django_summernote.widgets import SummernoteInplaceWidget
 
 class ChannelPostForm(forms.ModelForm):
+    """
+    This class is for the summernote editor form
+
+    Parameters:
+        super class: class the this class inherits from
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
@@ -24,3 +30,7 @@ class ChannelPostForm(forms.ModelForm):
     class Meta:
         model = ChannelPosts
         fields = ['post']
+
+# use for the tag drop down list
+# class AddUserToChannelForm(forms.Form):
+#     user = forms.ModelChoiceField(queryset=User.objects.all(), label='Select User')
--- channels/models.py
+++ channels/models.py
@@ -2,30 +2,47 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_summernote.fields import SummernoteTextField
 
+
 class ChannelModel(models.Model):
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='%(class)s_created')
+    """
+    This class is for each indivual channel that is created 
+    and the fields to be added in the the database
+
+    Parameters:
+        super class: class the this class inherits from
+    """
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.SET_NULL,
+                                   null=True, related_name='%(class)s_created')
     created_date = models.DateTimeField(auto_now_add=True)
     is_private = models.BooleanField(default=True)
     name = models.CharField(max_length=254,default='')
     description = models.TextField(blank=True, null=True)
-    
-    
+    users = models.ManyToManyField(User, related_name='channels')
+
     def __str__(self):
         return self.name
-    
+
 
 class ChannelPosts(models.Model):
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='%(class)s_created')
+    """
+    This class is for each indivual post in a chanel
+
+    Parameters:
+        super class: class the this class inherits from
+    """
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.SET_NULL,
+                                   null=True, related_name='%(class)s_created')
     created_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=254,default='')
-    post = SummernoteTextField(default='')  # Supports rich text, including images
-    # ForeignKey to associate posts with channels
+    # Supports rich text, including images
+    # ForeignKey to associate posts with channel
+    post = SummernoteTextField(default='')
+
     post_channel = models.ForeignKey(
         ChannelModel,
         on_delete=models.SET_NULL,
         null=True,
         related_name='posts_created'
     )
-
-
-
