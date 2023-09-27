1a209572b4ea32b91d4996b40c4fe3f2bd0428bf
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-27 20:33:16 +0100

    Chore: add view for adding user to a channel

--- channels/views.py
+++ channels/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render,get_object_or_404,redirect
+from django.urls import reverse
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 from .models import ChannelModel,ChannelPosts
 from .forms import ChannelPostForm
-from django.urls import reverse
+from django.http import JsonResponse
+
+
 
 
 def view_channels(request):
-    """ A view that renders the channels  page """
+    """ A view that renders the channels page  with a list of channels"""
 
     channels = ChannelModel.objects.all()
 
@@ -18,17 +23,28 @@ def view_channels(request):
 
 
 def channel_posts(request, channel_id):
-    """ A view that renders the posts contents page for a specific channel """
-    
+    """
+    A view that renders the posts contents page for a specific channel
+
+    Parameters:
+        request: object provides important information about the incoming request.
+        channel_id (int): The channel id the user is being added to
+
+    Returns:
+        request: object provides important information about the response
+        html: channels/posts.html
+        context: extra variables in a dictionary
+
+    """
     # Get the specific channel or return a 404 if it doesn't exist
     channel = get_object_or_404(ChannelModel, id=channel_id)
 
     if request.method == 'POST':
         form = ChannelPostForm(request.POST)
         if form.is_valid():
-            # Add the channel information to the post (you can change this logic)
+            # Add the channel information to the post
             form.instance.post_channel = channel
-            
+        
             post = form.save(commit=False)
             post.created_by = request.user
             post.post_channel_id = channel_id
@@ -44,8 +60,39 @@ def channel_posts(request, channel_id):
     context = {
         'channel': channel,
         'posts': posts,
-        'form': form
+        'form': form,
+        'channel_users': channel.users.all(),  # Include user data
+
     }
 
     return render(request, 'channels/posts.html', context)
 
+
+
+
+def add_user_to_channel(request, channel_id, user_id):
+    """
+    This function adds a user to a channel by adding there 
+    user object to the channelmodel class: users.
+
+    Parameters:
+        request: object provides important information about the incoming request.
+        channel_id (int): The channel id the user is being added to
+        user_id (int): The id of the user being added
+
+    Returns:
+        json object: JsonResponse message success or error
+    """
+    try:
+        # Get the channel and user objects
+        channel = get_object_or_404(ChannelModel,id=channel_id)
+        user = get_object_or_404(User,id=user_id)
+
+        # Add the user to the channel
+        channel.users.add(user)
+
+        # Return a JSON response for success
+        return JsonResponse({'success': f'{user.username} added to channel'})
+    except Exception:
+        # Return a JSON response for channel not add user
+        return JsonResponse({'error': f'Could not add {user.username}'})
