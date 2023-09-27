130c0136fee0fe772b8f567d3869832bf6c181fb
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-27 20:34:17 +0100

    Chore: add url for add user to channel

--- channels/urls.py
+++ channels/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+# urls to the views functions
 urlpatterns = [
     path('', views.view_channels, name='channels'),
     path('channel_posts/<int:channel_id>/', views.channel_posts, name='channel_posts'),
+    path('add_user_to_channel/<int:channel_id>/<int:user_id>/', views.add_user_to_channel, name='add_user_to_channel'),
 ]
