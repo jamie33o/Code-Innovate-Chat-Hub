3dfd408986508f1d0a5ca2b33fbff0de07f32bce
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-09-28 14:26:33 +0100

    Refactor: remov crispy forms

--- channels/forms.py
+++ channels/forms.py
@@ -1,35 +1,23 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Row, Column
-from crispy_forms.bootstrap import FormActions
 from django import forms
-from .models import ChannelPosts
 from django_summernote.widgets import SummernoteInplaceWidget
+from .models import ChannelPosts
 
-class ChannelPostForm(forms.ModelForm):
+class ChannelPostForm(forms.ModelForm): 
     """
     This class is for the summernote editor form
 
     Parameters:
         super class: class the this class inherits from
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Row(
-                Column('post', css_class='col-md-6'),
-            ),
-            FormActions(
-                Submit('submit', 'Submit', css_class='btn btn-primary')
-            )
-        )
-
-        # Add Summernote widget to the 'post' field
-        self.fields['post'].widget = SummernoteInplaceWidget(attrs={'class': 'summernote'})
-
     class Meta:
         model = ChannelPosts
         fields = ['post']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['post'].widget = SummernoteInplaceWidget(attrs={'class': 'summernote'})
+
+
 
 # use for the tag drop down list
 # class AddUserToChannelForm(forms.Form):
