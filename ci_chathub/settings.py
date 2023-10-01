865ad214ddb6918a7712aeecfe08dab97443efb8
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-01 16:10:22 +0100

    Fix: update databse settings

--- ci_chathub/settings.py
+++ ci_chathub/settings.py
@@ -154,7 +154,7 @@ WSGI_APPLICATION = 'ci_chathub.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('DATABASE_URL')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
