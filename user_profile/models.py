3884cf6fb1d2a1deffd4ef532b49d0f6dab48b97
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-09 12:52:02 +0100

    Chore: add last view channel_id to model
    changed import of User to get_user_model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 class UserProfile(models.Model):
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     is_active = models.BooleanField(null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    #image_url = models.URLField(max_length=1024, null=True, blank=True)
+    last_viewed_channel_id = models.IntegerField(blank=True, null=True)
+
 
     def __str__(self):
         return self.user.username
