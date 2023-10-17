579812960a7457259d74931de986e83c5f41f65a
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-17 23:12:49 +0100

    Chore: add comments consumer
    Refactored the consumers.py by creating baseConsumer class and inherited from it for postconsumer and commentconsumer

--- home/consumers.py
+++ home/consumers.py
@@ -1,27 +1,42 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class PostConsumer(AsyncWebsocketConsumer):
+class BaseConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Extract channel ID from URL parameters
-        self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
-        self.room_group_name = f"post_channel_{self.channel_id}"
-        # Join room group
+        self.room_group_name = self.get_room_group_name()
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
+    def get_room_group_name(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
     async def post_notification(self, event):
-        # Send post notification to WebSocket
         await self.send(text_data=json.dumps({
+            'type': 'post_notification',
             'message': event['message'],
             'post_content': event['post_content'],
             'post_creator': event['post_creator'],
         }))
+
+    async def comment_notification(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'comment_notification',
+            'message': event['message'],
+            'comment_content': event['comment_content'],
+            'comment_creator': event['comment_creator'],
+        }))
+class PostConsumer(BaseConsumer):
+    def get_room_group_name(self):
+        self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
+        return f"post_channel_{self.channel_id}"
+
+class CommentConsumer(BaseConsumer):
+    def get_room_group_name(self):
+        self.post_id = self.scope["url_route"]["kwargs"]["post_id"]
+        return f"comment_post{self.post_id}"
 
    
       
