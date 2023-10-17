6abe7eb19336941fb72cfbdb2b5ffde2cc3bd82d
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-17 23:13:51 +0100

    Chore: add url for comments consumer

--- home/routing.py
+++ home/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/channel_posts/(?P<channel_id>\d+)/$', consumers.PostConsumer.as_asgi()),
+    re_path(r'ws/post_comments/(?P<post_id>\d+)/$', consumers.CommentConsumer.as_asgi()),
 ]
