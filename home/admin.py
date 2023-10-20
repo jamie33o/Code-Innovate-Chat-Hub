52a4a45e63a7d1882e6a190f0c60785267ac2043
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-20 22:13:40 +0100

    Chore: add name to admin url and add channelposts model and postcomments model to admin

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import ChannelModel
+from .models import ChannelModel,PostComments,ChannelPosts
 
 admin.site.register(ChannelModel)
+admin.site.register(PostComments)
+admin.site.register(ChannelPosts)
