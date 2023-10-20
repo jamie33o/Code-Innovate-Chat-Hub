4b65969edd96c746eebc92eff41e95eef0584540
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-20 18:17:00 +0100

    Chore: add email settings

--- ci_chathub/settings.py
+++ ci_chathub/settings.py
@@ -138,7 +138,6 @@ AUTHENTICATION_BACKENDS = [
 # but you can configure and manage multiple sites within a single Django project if needed.
 SITE_ID = 1
 
-EMAIL_BACKENDS = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 # These three email settings here make it so that an email is required to register for the site.
@@ -243,3 +242,13 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
