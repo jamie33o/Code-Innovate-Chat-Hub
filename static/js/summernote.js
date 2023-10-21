d3cb645dc0465136da91a959bf513d5f8f115d81
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-21 20:11:35 +0100

    Style: add upload image function
    Added upload image function which uploads the image straight away when the user choose an imag then the urls are returned and added when submitting the the form, also add a listener for when the editor is in focus or not and change its opacity,  added validation check to the editor if its empty and the user tries to submit it will display error message

--- static/js/summernote.js
+++ static/js/summernote.js
@@ -2,6 +2,7 @@ class SummernoteEnhancer {
 
   static sharedChannelUsers= [];
   static emojiImgs = null;
+  static imgUrl = null;
 
   constructor() {
     this.divToLoadIn = null;
@@ -11,6 +12,7 @@ class SummernoteEnhancer {
     this.snForm = null;
     this.divsection = null;
     this.$storeText = null;
+    this.uploadImageUrls = [];
   }
 
 
@@ -20,6 +22,8 @@ class SummernoteEnhancer {
     this.divToLoadIn = divToLoadIn
     this.djangoUrl = djangoUrl
     this.$sn = $(`${this.divToLoadIn} textarea`);  
+
+
 
     document.emojiSource = SummernoteEnhancer.emojiImgs;
 
@@ -41,16 +45,33 @@ class SummernoteEnhancer {
               self.$sn.next().find('.note-editable').on('input', function () {
                   self.tagUser(false);
               });
+          },
+          // image upload callback 
+          onImageUpload: function(files) {
+            self.uploadImage(files[0],self.$sn);
           }
         }
 
     });
-    this.snSubmitForm = $(`${this.divToLoadIn} .sn-submit-btn`)
-    this.snForm = $(`${this.divToLoadIn} .sn-form`)
 
+    // summernote submit button
+    this.snSubmitForm = $(`${this.divToLoadIn} .sn-submit-btn`)
+    // summernote form 
+    this.snForm = $(`${this.divToLoadIn} .sn-form`)
+    // submit btn listener
     this.snSubmitForm.click((event) => {
             event.preventDefault();
-            this.submitForm(this.djangoUrl);
+            // Check if the content is empty or meets your criteria
+            let $summernoteContent = self.$sn.summernote('code');
+            let tempDiv = $('<div>');
+            tempDiv.html($summernoteContent);
+            
+            let plainText = tempDiv.text().trim();
+            if (!plainText) {
+              displayMessage({'status': 'summernote-error'})
+            }else{
+              this.submitForm(self.djangoUrl);
+            }
         });
 
     this.divsection = $(this.divToLoadIn)
@@ -63,6 +84,15 @@ class SummernoteEnhancer {
       this.tagUser();      
 
     });
+    // fading summerote buttons
+    $(`${divToLoadIn} .note-editable`).on('focus', function() {
+        $(`${divToLoadIn} .btn-group button, ${divToLoadIn} .note-placeholder, ${divToLoadIn} .note-toolbar`).removeClass('faded');
+    });
+
+    $(`${this.divToLoadIn} .note-editable`).on('blur', function() {
+        $(`${divToLoadIn} .btn-group button, ${divToLoadIn} .note-placeholder, ${divToLoadIn} .note-toolbar`).addClass('faded');
+
+    });
 
     //overlay event listener to close modals
     $(`${this.divToLoadIn} .hide-modal`).click(() => {
@@ -70,10 +100,10 @@ class SummernoteEnhancer {
       $(`${this.divToLoadIn} .hide-modal`).hide();
       emojiPicker.$panel.hide();
     });
+
     let emojiPicker = new EmojiPicker(this.divToLoadIn, function(emoji){
         self.$sn.summernote('editor.insertNode', emoji);
         $(`${self.divToLoadIn} .hide-modal`).hide()
-
     });
 
     // Add this code to bind the click event of your existing button
@@ -132,10 +162,13 @@ class SummernoteEnhancer {
   }
 
   submitForm(djangoUrl){
+    const self = this;
     // Serialize the form data
-    const formData = this.snForm.serialize();
-     // Preserve the reference to the class instance
-     const self = this;
+    let formData = this.snForm.serialize();
+    $.each(self.uploadImageUrls, function(index, url) {
+      formData += `&urls[]=${encodeURIComponent(url)}`;
+  });
+    // Preserve the reference to the class instance
     // Send the form data to Django
     $.ajax({
         url: djangoUrl,  // Replace with your Django view URL
@@ -152,6 +185,34 @@ class SummernoteEnhancer {
     });
 
   }
+
+  uploadImage(file, editor, welEditable) {
+      // Create a FormData object to send the file to the server
+      var formData = new FormData();
+      formData.append("file", file);
+      let self = this
+  
+      // Send the file to the server using AJAX
+      $.ajax({
+        data: formData,
+        type: "POST",
+        url: 'upload-image/',  
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function(response) {
+         
+          $('#channel-posts div.note-editing-area').append('<img src="' + response.url + '" alt="Uploaded Image">');
+          self.uploadImageUrls.push(response.url)
+         
+          self.$sn.summernote('focus');
+
+        },
+        error: function(error) {
+          console.error("Error uploading image:", error);
+        }
+      });
+    }
 
 
 }
