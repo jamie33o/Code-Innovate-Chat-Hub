295883daa5c3a3e080ec6cecc1a6b3fac3bceef4
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-25 16:14:47 +0100

    Chore: change time settings to irish time

--- ci_chathub/settings.py
+++ ci_chathub/settings.py
@@ -196,9 +196,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-ie'
+
+TIME_ZONE = 'Europe/Dublin'
 
 USE_I18N = True
 
