297a32ed8a574db752918bab0f9c9272d36e8b80
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-10-29 13:44:56 +0000

    Chore: add url for editing posts

--- home/urls.py
+++ home/urls.py
@@ -10,6 +10,7 @@ from .views import (HomeView,
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('channel_posts/<int:channel_id>/', ChannelPostsView.as_view(), name='channel_posts'),
+    path('channel_posts/<int:channel_id>/<int:post_id>/', ChannelPostsView.as_view(), name='channel_posts'),
     path('post_comments/<int:post_id>/', PostCommentsView.as_view(), name='post_comments'),
     path('add_user_to_channel/<int:channel_id>/<int:user_id>/', AddUserToChannelView.as_view(), name='add_user_to_channel'),
     path('upload-image/', ImageUploadView.as_view(), name='image_upload'),
