4d185668fdfeff6383b8e9380a9bd79f8f93c224
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-08 17:42:25 +0000

    Refactor: remove signal for resizing images

--- group_chat/apps.py
+++ group_chat/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 class ChannelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'group_chat'
-
-
-    def ready(self):
-        import group_chat.signals
