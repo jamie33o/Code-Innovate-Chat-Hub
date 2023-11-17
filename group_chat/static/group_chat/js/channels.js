bdcc2f1118ecb80836e2b6e76679cc36fb021f6a
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-17 10:44:00 +0000

    Chore: add docstring and remove unused code

--- group_chat/static/group_chat/js/channels.js
+++ group_chat/static/group_chat/js/channels.js
@@ -1,52 +1,43 @@
-// Toggle the visibility of the messages list when the button is clicked
+/**
+ * Toggle the visibility of the messages list when the button is clicked.
+ * 
+ * This function handles the click event on elements with the class 'toggleLinksButton'.
+ * It toggles the visibility of the target element specified by the 'data-target' attribute
+ * and rotates the icon inside the clicked button.
+ */
 $('.toggleLinksButton').click(function() {
     var target = $(this).data('target');
 
-    $(target).toggleClass('d-none')
+    $(target).toggleClass('d-none');
     $(this).find('i').toggleClass('fa-rotate-90');
-
 });
 
-// Use the ready function to execute code when the DOM is fully loaded
-// Click event for the button
+/**
+ * 
+ * This function handles the click event on channel links 
+ * It prevents the default behavior of the link, updates the active channel, and performs an AJAX request
+ * to load the channel posts.
+ */
 $('main').on('click', '.channel-link', function(event) {
     event.preventDefault();
-    
-    $('.channel-link').removeClass('active')
 
-    $(this).find('.unread-indicator').remove()
-    $(this).addClass('active')
-    lastChannel = $(this)
-    let url = event.currentTarget.href
+    $('.channel-link').removeClass('active');
+
+    $(this).find('.unread-indicator').remove();
+    $(this).addClass('active');
+    lastChannel = $(this);
+    let url = event.currentTarget.href;
     ajaxRequest(url, null, 'Get', '#channel-posts', null, function(response){
-        $('#channel-posts').html(response)
-        autoScroll()
-    } )
-   
+        $('#channel-posts').html(response);
+        autoScroll();
+    });
 
     if(window.innerWidth < 575){
         $('#channel-posts').addClass('d-flex');
         $('#channel-links-container').addClass('d-none');
-        $('#nav-bar').addClass('d-none')
-        $('header').addClass('d-none')
-
+        $('#nav-bar').addClass('d-none');
+        $('header').addClass('d-none');
     }
     $('#channel-posts').addClass('d-flex');
-
     $('#post-comments').removeClass('d-flex');
 });
-
-
-
-/////////////////code to add user to channel with ajax////////////////////
-// this is a button overlay that shows when user is not added to a the channel
-const addChannelButton = document.getElementById('add-channel-button');
-const overlay = document.getElementById('overlay');
-
-if (addChannelButton && overlay) {
-    addChannelButton.addEventListener('click', function() {
-        overlay.style.display = 'block';
-    });
-  }
-
-
