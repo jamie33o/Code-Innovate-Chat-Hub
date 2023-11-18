3ac86dd7b023b0b974180f194e81a326ca4090ba
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-18 10:14:52 +0000

    Chore: initialize signals in apps.py

--- messaging/apps.py
+++ messaging/apps.py
@@ -1,6 +1,9 @@
+# apps.py
 from django.apps import AppConfig
-
 
 class MessagingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'messaging'
+
+    def ready(self):
+        import messaging.signals
