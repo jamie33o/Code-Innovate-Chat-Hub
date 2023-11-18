926c2ec3d266c30817c96351b338db302ac3aacf
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-18 10:07:41 +0000

    Chore: create message model and unreadmessage model

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,18 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender} to {self.receiver} - {self.timestamp}"
+
+class UnreadMessage(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='unread_messages')
+
