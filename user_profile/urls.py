d7192d912b911023fafb634579667d06e8825ecd
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-18 17:12:57 +0000

    Style: url for viewing other users profile

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,11 +1,12 @@
 """URL patterns for user profile-related views."""
 
 from django.urls import path
-from .views import UserProfileView, update_profile_image, update_status
+from .views import UserProfileView, ViewUserProfile, update_profile_image, update_status
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='user_profile'),
     path('update_profile_image/', update_profile_image, name='update_profile_image'),
     path('update_status/', update_status, name='update_status'),
+    path('view_user_profile/<int:user_id>/', ViewUserProfile.as_view(), name='view_user_profile'),
 ]
 
