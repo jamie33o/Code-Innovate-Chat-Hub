4df6cdd4847bde9e231606bd7a1ee8ef7a59d234
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-19 01:43:15 +0000

    Chore: update the views for sending message and adding to conversation

--- messaging/views.py
+++ messaging/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from django.contrib.auth.models import User
-from .models import Message, UnreadMessage
+from .models import Message, UnreadMessage, Conversation
 from django.utils.decorators import method_decorator
 from django.db import models  
-from django.db.models import Q
 from datetime import datetime
-
 
 
 
@@ -18,43 +16,61 @@ class InboxView(View):
 
     def get(self, request, receiver_id=None):
         # Retrieve received messages and unread messages for the user
-        message_exists = None
-            
-        
-        messages = Message.objects.filter(
-                Q(sender=request.user) | Q(receiver=request.user)
-        ).order_by('sender', 'receiver', '-timestamp').distinct('sender', 'receiver')
+        message_exists = True
 
         unread_messages = UnreadMessage.objects.filter(user=request.user)
+        
+        # Retrieve conversations involving the current user
+        conversations = Conversation.objects.filter(participants=request.user)
+        
+        if not conversations.exists():
+            # Create a new conversation if none exists
+            conversation = Conversation.objects.create()
+            conversation.participants.add(request.user)
+        else:
+            # Use the first existing conversation
+            conversation = conversations.first()
+
+        # Retrieve the messages in the conversation
+        messages_by_conversation = []
+
+        # Retrieve the latest message in the conversation
+        latest_message = conversation.messages.order_by('-timestamp').first()
+
+        # Check if there is a latest message before adding it to the list
+        if latest_message:
+            messages_by_conversation.append(latest_message)
 
         if receiver_id:
-            receiver= get_object_or_404(User, id=receiver_id)
-            message_exists = any(receiver_id is message.sender.id or receiver_id is message.receiver.id for message in messages)
-            if not message_exists:
-                # Create a new Message object
-                new_message = Message(
-                    sender=request.user,  # Set the sender
-                    receiver= receiver,
-                    timestamp=datetime.now(),
-                    content="New message!",
-                )
-        
+            receiver = get_object_or_404(User, id=receiver_id)
 
-                # Append the new_message object to the list
-                messages = list(messages)
-                messages.append(new_message)
+            # Add participants to the conversation if not already there
+            if receiver not in conversation.participants.all():
+                conversation.participants.add(receiver)
+
+            # Create a new Message
+            new_message = Message.objects.create(
+                sender=request.user,
+                receiver=receiver,
+                content='new message',
+                timestamp=datetime.now(),
+                conversation=conversation
+            )
+
+            messages_by_conversation.append(new_message)
 
         context = {
-            'messages': messages,
+            'messages': messages_by_conversation,
             'unread_messages': unread_messages,
         }
-        if not message_exists and receiver_id:
+
+        if message_exists and receiver_id:
             context['new_message'] = True
         elif receiver_id:
             context['receiver'] = receiver
 
         return render(request, self.template_name, context)
-    
+
 
 
 @method_decorator(login_required, name='dispatch')
@@ -65,11 +81,13 @@ class MessageListView(View):
         sender = get_object_or_404(User, id=sender_id)
         receiver = get_object_or_404(User, id=receiver_id)
 
-        # Retrieve messages between the sender and receiver
-        messages = Message.objects.filter(
-            (models.Q(sender=sender) & models.Q(receiver=receiver)) |
-            (models.Q(sender=receiver) & models.Q(receiver=sender))
-        ).order_by('timestamp')
+        # Find the conversation involving both the sender and receiver
+        conversation = Conversation.objects.filter(participants=request.user).filter(participants=receiver).first()
+
+        # Check if the conversation exists
+        if conversation:
+            # Retrieve messages in the conversation and order by timestamp
+            messages = conversation.messages.all().order_by('timestamp')
 
         context = {
             'sender': sender,
@@ -83,16 +101,18 @@ class MessageListView(View):
 
 @method_decorator(login_required, name='dispatch')
 class SendMessageView(View):
-    template_name = 'messaging/send_message.html'
 
     def post(self, request, receiver_id):
         receiver = User.objects.get(pk=receiver_id)
-        content = request.POST.get('content', '')
-
+        content = request.POST.get('post', '')
         if content:
-            message = Message(sender=request.user, receiver=receiver, content=content)
+            conversation = Conversation.objects.filter(participants=request.user).filter(participants=receiver).first()
+
+            # Create a new message
+            message = Message(sender=request.user, receiver=receiver, content=content,conversation=conversation)
             message.save()
 
+         
         return redirect('inbox')
 
 
