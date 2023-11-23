f83265717169e32944d248fc7f50dedf0e67f5b8
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-23 01:36:25 +0000

    Style: add styling to saved posts section

--- messaging/urls.py
+++ messaging/urls.py
@@ -4,9 +4,12 @@ from .views import InboxView, MessageListView, GenericObjectDeleteView
 
 urlpatterns = [
     path('', InboxView.as_view(), name='inbox'),
-    path('new_message/<int:receiver_id>/', InboxView.as_view(), name='new_message'),
-    path('send_message/<int:receiver_id>/', MessageListView.as_view(), name='send_message'),
-    path('message_list/<int:sender_id>/<int:receiver_id>/', MessageListView.as_view(), name='message_list'),
+    path('new_message/<int:receiver_id>/', 
+         InboxView.as_view(), name='new_message'),
+    path('send_message/<int:receiver_id>/', 
+         MessageListView.as_view(), name='send_message'),
+    path('message_list/<int:sender_id>/<int:receiver_id>/', 
+         MessageListView.as_view(), name='message_list'),
     path('delete_obj/<str:model>/<int:pk>/',
-          GenericObjectDeleteView.as_view(), name='delete_obj'),
+         GenericObjectDeleteView.as_view(), name='delete_obj'),
 ]
