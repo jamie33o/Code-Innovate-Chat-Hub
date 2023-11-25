701ddffb6d459130639effc44068d4b3776429c3
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-11-25 17:30:29 +0000

    Refactor: use imporlib to delay import

--- ci_chathub/routing.py
+++ ci_chathub/routing.py
@@ -9,13 +9,14 @@ WebSocket URL Patterns:
 - ws/channel_posts/(?P<channel_id>\d+)/$: Connects to `PostConsumer` for channel posts.
 - ws/post_comments/(?P<post_id>\d+)/$: Connects to `CommentConsumer` for post comments.
 """
+import importlib
 from django.urls import re_path
 from group_chat import consumers as gc_consumer
 from messaging import consumers as msg_consumer
-#from .consumers import GlobalConsumer
+
 websocket_urlpatterns = [
     re_path(r'ws/channel_posts/(?P<channel_id>\d+)/$', gc_consumer.PostConsumer.as_asgi()),
     re_path(r'ws/post_comments/(?P<post_id>\d+)/$', gc_consumer.CommentConsumer.as_asgi()),
     re_path(r'ws/messaging/(?P<conversation_id>\d+)/$', msg_consumer.MessageConsumer.as_asgi()),
-    #re_path(r'ws/global_consumer/(?P<user_id>\d+)/$', GlobalConsumer.as_asgi()),
+    re_path(r'ws/global_consumer/(?P<user_id>\d+)/$', importlib.import_module('path.to.GlobalConsumer').as_asgi()),
 ]
