2483c0e4c58f653cf674c1657a98791538052f17
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-02 03:18:10 +0000

    Refactor: add missing forward slash and remove empty space

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,9 +1,9 @@
 """URL patterns for user profile-related views."""
 
 from django.urls import path
-from .views import (UserProfileView, 
-                    ViewUserProfile, 
-                    update_profile_image, 
+from .views import (UserProfileView,
+                    ViewUserProfile,
+                    update_profile_image,
                     update_status,
                     remove_saved_post,
                     get_all_users_profiles)
@@ -15,6 +15,6 @@ urlpatterns = [
     path('view_user_profile/<int:user_id>/', ViewUserProfile.as_view(), name='view_user_profile'),
     path('edit_user_profile/', ViewUserProfile.as_view(), name='edit_user_profile'),
     path('remove_saved_post/<int:post_id>/', remove_saved_post, name='remove_saved_post'),
-    path('get_all_user_profiles', get_all_users_profiles, name='get_all_user_profiles'),
+    path('get_all_user_profiles/', get_all_users_profiles, name='get_all_user_profiles'),
 ]
 
