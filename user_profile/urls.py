70602cc40f35d8f2ba5180b09c7bc83516aff1f3
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-11 22:46:31 +0000

    Chore: add contact view url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,15 +7,17 @@ from .views import (UserProfileView,
                     update_status,
                     remove_saved_post,
                     get_all_users_profiles,
-                    delete_user_account)
+                    delete_user_account,
+                    contact_view)
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='user_profile'),
     path('update_profile_image/', update_profile_image, name='update_profile_image'),
     path('update_status/', update_status, name='update_status'),
     path('view_user_profile/<int:user_id>/', ViewUserProfile.as_view(), name='view_user_profile'),
-    path('edit_user_profile/', ViewUserProfile.as_view(), name='edit_user_profile'),
+    path('edit_user_profile/', UserProfileView.as_view(), name='edit_user_profile'),
     path('remove_saved_post/<int:post_id>/', remove_saved_post, name='remove_saved_post'),
     path('get_all_user_profiles/', get_all_users_profiles, name='get_all_user_profiles'),
-    path('delete_account/', delete_user_account, name='delete_account')
+    path('delete_account/', delete_user_account, name='delete_account'),
+    path('contact/', contact_view, name='contact'),
 ]
