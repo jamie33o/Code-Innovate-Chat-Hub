f01d9e04b3ebe73cab921c54b4e382ca7a5fe395
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-13 13:07:01 +0000

    Style: minor style changes

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,7 +1,9 @@
 """
     The admin.py file in a Django app is used to define the 
-    configuration for the Django admin interface related to the models in that app. 
-    This file allows you to customize how the app's models are displayed and managed through the Django admin.
+    configuration for the Django admin interface related
+    to the models in that app. 
+    This file allows you to customize how the app's
+    models are displayed and managed through the Django admin.
 """
 from django.contrib import admin
 from .models import UserProfile
