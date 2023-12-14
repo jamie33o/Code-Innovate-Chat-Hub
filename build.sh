38c148a4621a46cdd5d874920a3ec54d8a71ae1a
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-14 23:45:15 +0000

    Chore: add build script for minify static files and add favicon

---
+++ build.sh
@@ -0,0 +1,32 @@
+# Get Django settings using a management command
+PROJECT_DIR=$(python manage.py shell -c "from django.conf import settings; print(settings.BASE_DIR)")
+
+# Define an array of app names
+APPS=("group_chat" "messaging" "user_profile")
+
+# Minify CSS and JS files for each app
+for app_name in "${APPS[@]}"; do
+  # Minify CSS files for the current app
+  find "$PROJECT_DIR/$app_name/static/$app_name/css/" -type f -name '*.css' -print0 | while IFS= read -r -d $'\0' css_file; do
+    # Minify CSS and overwrite the original file
+    cleancss "$css_file" -o "$css_file"
+  done
+
+  # Minify JS files for the current app
+  find "$PROJECT_DIR/$app_name/static/$app_name/js/" -type f -name '*.js' -print0 | while IFS= read -r -d $'\0' js_file; do
+    # Minify JS and overwrite the original file
+    uglifyjs "$js_file" -o "$js_file"
+  done
+done
+
+# Minify CSS and JS files for the root level static folder
+find "$PROJECT_DIR/static/" -type f \( -name '*.css' -o -name '*.js' \) -print0 | while IFS= read -r -d $'\0' file; do
+  # Minify CSS or JS based on file extension and overwrite the original file
+  if [[ "$file" == *.css ]]; then
+    cleancss "$file" -o "$file"
+  elif [[ "$file" == *.js ]]; then
+    uglifyjs "$file" -o "$file"
+  fi
+done
+
+
