6c7965854026f986be5a486596cd4c71b63cbcf0
jamie33o <123118240+jamie33o@users.noreply.github.com>
2023-12-15 17:57:44 +0000

    Chore: load scripts asynchronously for performance
    also  add bash script to minify static files

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 # Get Django settings using a management command
-PROJECT_DIR='https://ci-chathub-bucket.s3.amazonaws.com'
-
+PROJECT_DIR=$(python manage.py shell -c "from django.conf import settings; print(settings.BASE_DIR)")
+echo $PROJECT_DIR
 # Define an array of app names
 APPS=("group_chat" "messaging" "user_profile")
 
 # Minify CSS and JS files for each app
 for app_name in "${APPS[@]}"; do
   # Minify CSS files for the current app
-  find "$PROJECT_DIR/static/$app_name/css/" -type f -name '*.css' -print0 | while IFS= read -r -d $'\0' css_file; do
+  find "$PROJECT_DIR/$app_name/static/$app_name/css/" -type f -name '*.css' -print0 | while IFS= read -r -d $'\0' css_file; do
     # Minify CSS and overwrite the original file
     cleancss "$css_file" -o "$css_file"
   done
 
   # Minify JS files for the current app
-  find "$PROJECT_DIR/static/$app_name/js/" -type f -name '*.js' -print0 | while IFS= read -r -d $'\0' js_file; do
+  find "$PROJECT_DIR/$app_name/static/$app_name/js/" -type f -name '*.js' -print0 | while IFS= read -r -d $'\0' js_file; do
     # Minify JS and overwrite the original file
     uglifyjs "$js_file" -o "$js_file"
   done
@@ -28,5 +28,3 @@ find "$PROJECT_DIR/static/" -type f \( -name '*.css' -o -name '*.js' \) -print0 
     uglifyjs "$file" -o "$file"
   fi
 done
-
-
